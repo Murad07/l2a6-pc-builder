d355484328663d6664ad108da58899a6b72b8950
Murad07 <murad.pi22@gmail.com>
2023-07-31 14:29:59 +0600

    solve deployment issue

--- src/redux/api/api.js
+++ src/redux/api/api.js
@@ -3,7 +3,9 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 // Define our single API slice object
 export const apiSlice = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://l2a6-pc-builder-murad07.vercel.app/",
+  }),
   endpoints: (builder) => ({
     getProducts: builder.query({
       query: () => "/products",
